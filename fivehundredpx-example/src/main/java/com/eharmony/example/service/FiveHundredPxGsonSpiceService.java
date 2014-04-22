9c8e98cf18cb068e55fa6298f7d2f6fabbb2b5a8
Frederick Siu <fsiu@eharmony.com>
2014-04-21 19:23:22 -0700

    Added additional support files

---
+++ fivehundredpx-example/src/main/java/com/eharmony/example/service/FiveHundredPxGsonSpiceService.java
@@ -0,0 +1,31 @@
+package com.eharmony.example.service;
+
+import retrofit.RestAdapter;
+
+import com.octo.android.robospice.retrofit.RetrofitGsonSpiceService;
+
+import com.eharmony.example.service.catalog.UrlCatalog;
+import com.eharmony.example.service.client.FiveHundredPxClient;
+
+/**
+ * Created by fsiu on 3/21/14.
+ */
+public class FiveHundredPxGsonSpiceService extends RetrofitGsonSpiceService {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        addRetrofitInterface(FiveHundredPx.class);
+    }
+
+    @Override
+    protected String getServerUrl() {
+        return UrlCatalog.FIVE_HUNDRED_PX_URL;
+    }
+
+    @Override
+    protected RestAdapter.Builder createRestAdapterBuilder() {
+        return super.createRestAdapterBuilder().setClient(FiveHundredPxClient.INSTANCE.getClient());
+    }
+
+}
