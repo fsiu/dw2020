b2e9d2c90d0d83d56feddbcb200edb063df68f49
Frederick Siu <fsiu@eharmony.com>
2015-04-08 12:39:14 -0700

    Removed code that doesn't need to exist

--- dw2020/src/main/java/net/darkwire/example/widgets/adapter/PhotoAdapter.java
+++ dw2020/src/main/java/net/darkwire/example/widgets/adapter/PhotoAdapter.java
@@ -28,7 +28,7 @@ public class PhotoAdapter extends NumericPaginationBaseAdapter<FiveHundredPxPhot
 
     @Override
     public View getView(final int position, View convertView, final ViewGroup viewGroup) {
-        
+
         //View layout = R.layout.item
         //FiveHundredPxPhoto.getImageUrl() for the photo url
         //FiveHundredPxPhoto.getName() to setTextView() from the NumericPaginationBaseAdapter
